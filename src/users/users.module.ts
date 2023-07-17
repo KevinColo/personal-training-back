2dcf906b6f90d60c0630aa0ad20c194b4b75e641
kevincolonna <kevin.colonna@orange.com>
2023-07-17 10:01:46 +0200

    fix npm start

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -3,9 +3,10 @@ import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserRepository } from './user.repository';
+import { User } from './user.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([UserRepository])],
+  imports: [TypeOrmModule.forFeature([User, UserRepository])],
   providers: [UsersService],
   controllers: [UsersController],
 })
